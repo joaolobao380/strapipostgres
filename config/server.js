16f846771438adedf91f00cdd3ac51e68df81509
joaolobao380 <victorlobao380@gmail.com>
2021-09-05 23:18:35 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a5144821ead655814f09a8d80abdce9a'),
+    },
+  },
+});
